de3e7a5d23243b0d725baebaa3136a5ba2032a80
preetchani <preetchani23498@gmail.com>
2021-09-03 18:07:30 +0530

    react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Switch,Route } from 'react-router';
 import './App.css';
 import HomePage from './pages/homepage.component';
+
+const HatPage=()=>(
+<div>
+  <h1>Hats page</h1>
+</div>
+);
 
 function App() {
   return (
     <div>
-     <HomePage/>
+      <Switch>
+      <Route exact path='/' component={HomePage}/>
+      <Route path='/shop/hats' component={HatPage}/>
+      </Switch>
+     
     </div>
   );
 }
