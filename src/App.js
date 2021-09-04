4c207595ab5cfc43a431fb8208cc31a1bba5dfc3
preetchani <preetchani23498@gmail.com>
2021-09-04 19:40:13 +0530

    product preview and item component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import { Switch,Route } from 'react-router';
 import './App.css';
-import HomePage from './pages/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatPage=()=>(
-<div>
-  <h1>Hats page</h1>
-</div>
-);
 
 function App() {
   return (
     <div>
       <Switch>
       <Route exact path='/' component={HomePage}/>
-      <Route path='/shop/hats' component={HatPage}/>
+      <Route path='/shop' component={ShopPage}/>
       </Switch>
      
     </div>
