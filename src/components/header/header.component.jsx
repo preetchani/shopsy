21eccd1447c66a14c4d9fbc8d3ab044d8ae272ce
preetchani <preetchani23498@gmail.com>
2021-09-05 20:32:09 +0530

    Header logo updated to SVG

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './header.styles.scss';
-import Logo from '../../assets/images/logo.png';
+import {ReactComponent as Logo} from '../../assets/images/logo1.svg';
 import {Link} from 'react-router-dom';
 
 
 const Header = ()=>(
 <div className='header'>
     <Link className='logo-container' to='/'>
-        <img src={Logo} alt="" className='logo'/>
+        <Logo className='logo'/>
     </Link>
     <div className="options">
         <Link className='option' to='/'>
