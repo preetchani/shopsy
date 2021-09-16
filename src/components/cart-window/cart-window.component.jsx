aa0af84c273c9ecf8ab37e7137c3fb6723e1f56d
preetchani <preetchani23498@gmail.com>
2021-09-16 23:06:59 +0530

    Cart-item component created and connected to minicart view

--- src/components/cart-window/cart-window.component.jsx
+++ src/components/cart-window/cart-window.component.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import CustomButton from "../custom-button/custom-button.component";
 import './cart-window.styles.scss';
-const CartWindow = ()=>(
+import {connect} from 'react-redux';
+import CartItem from "../cart-item/cart-item.component";
+
+const CartWindow = ({cartItems})=>(
     <div className="cart-window">
-        <div className="cart-items"/>
+        <div className="cart-items">
+            {
+                cartItems.map(cartItem=>(
+                    <CartItem key={cartItem.id} item={cartItem}/>
+                ))
+            }
+        </div>
         <CustomButton>Go to Checkout</CustomButton>
     </div>
 );
 
-export default CartWindow;
+const mapStateToProps = ({cart:{cartItems}})=>({
+cartItems
+});
+
+export default connect(mapStateToProps)(CartWindow);
